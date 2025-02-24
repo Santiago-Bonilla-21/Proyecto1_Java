284e289e6f8c521729bdb7aedda6f0a83903dc7b
Santiago-Bonilla-21 <smbonilla@ucundinamarca.edu.co>
2025-02-24 11:40:51 -0500

    otro cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,3 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
